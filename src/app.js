107db0d0beda1750f7146374162a84e7d609ab1d
TwistedPorkchop <djerome99@laposte.net>
2022-08-12 21:54:00 +0200

    Couldnt fingure out the async issue when trying to draw edges. remade the original syncronous method, but checking for IDs takes a lot of time. So now im testing trying to place all nodes and delagates into seperate arrays to be able to batch create the nodes and edges.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 //Imports
 import { ApiPromise, WsProvider } from '@polkadot/api';
-import { u8aToString, hexToString } from '@polkadot/util';
+import { hexToString } from '@polkadot/util';
 
 var cytoscape = require('cytoscape');
 let cola = require('cytoscape-cola');
@@ -80,21 +80,28 @@ async function checkID(node_point) {
   //Checking if there are usernames to use the nodes
 
   const reg = /(^[0x])\w/g;
-  users = api.query.identity.identityOf(node_point)
+  users = api.query.identity.identityOf(node_point);
   
+  try {
   return users.then(identity => {
     
-    if (identity.toHuman() != null && identity.toHuman()["info"]["display"]["Raw"] != undefined) {
-      if (reg.test(identity.toHuman()["info"]["display"]["Raw"]) == true) {
-        return hexToString(identity.toHuman()["info"]["display"]["Raw"]);
+    if (identity.toHuman() != null ) {
+      if (identity.toHuman()["info"]["display"]["Raw"] != undefined){
+        if (reg.test(identity.toHuman()["info"]["display"]["Raw"]) == true) {
+          return hexToString(identity.toHuman()["info"]["display"]["Raw"]);
 
-      }else{
-        return identity.toHuman()["info"]["display"]["Raw"];
+        }else{
+          return identity.toHuman()["info"]["display"]["Raw"];
 
+        }
       }
     }
   return node_point;
   })
+  } catch (error) {
+    console.log(error);
+  }
+  
 }
 
 async function nodesCreation(){
@@ -104,18 +111,20 @@ async function nodesCreation(){
 
   addNodePromises = [];
   var i = 0;
+  //cy.startBatch();
   for (nodevar in nodes) {
-
+    
       addNodePromises.push(nodePromise = async() => {
           const node = nodevar
           const node_point = nodes[node][0].toHuman()[0]; //nodes in graph
           const edges = nodes[node][1][0].toHuman(); //node edges/graph connections
-
+          const id = await checkID(node_point);
+          console.log("here");
           //Adding node points    
           cy.add([{
               group: "nodes",
               data: {
-                  id: await checkID(node_point)
+                  id: id
               },
               position: {
                   x: 0,
@@ -136,26 +145,17 @@ async function nodesCreation(){
                   }
               }, ]);
           }
-
-          //Adding edges
-          
-          for (proxy of edges) {
-            //console.log(i);
-              cy.add([{
-                  group: "edges",
-                  data: {
-                      id: proxy.proxyType + i++ + "\n",
-                      source: await checkID(node_point),
-                      target: await checkID(proxy.delegate)
-                  }
-              }, ]);
-          }
-
       } //end promise
       );
-
+      
   } //end for loop
-  await Promise.all(addNodePromises);
+
+  await Promise.all(addNodePromises).then(()=> {
+    console.log("Nodes added");
+    //cy.endBatch();
+  }).catch(()=> {
+    console.log("Error")
+  });
 }
 async function edgeCreation(){
   api = await apiPromise;
@@ -188,14 +188,105 @@ async function edgeCreation(){
     } //end promise
     );
 
-} //end for loop
+  } //end for loop
+  await Promise.all(addNodePromises).then(()=> {
+    console.log("Edges added");
+  }).catch(()=> {
+    console.log("Error");
+  });
 }
 
 async function draw() {
   
-  await nodesCreation();
-  await edgeCreation();
-  
+  api = await apiPromise;
+  nodes = await api.query.proxy.proxies.entries();
+  proxy_actions = await api.query.proxy.announcements.entries();
+
+  identity = api.query.identity.identityOf(node_point);
+  var Nodes = [];
+  var Delegates = [];
+  var Edges = [];
+  var i =0;
+  for (nodevar in nodes) {
+    const node_point = nodes[node][0].toHuman()[0]; //nodes in graph
+    const edges = nodes[node][1][0].toHuman(); //node edges/graph connections
+
+    //Adding node points    
+    Nodes.push(node_point);
+
+    //And here the deleates
+    for (proxy of edges) {
+      Delegates.push(proxy.delegate);
+      Edges.push(i);
+    }
+    i++;
+  }//end for loop
+
+  if (identity.toHuman() != null ) {
+    if (identity.toHuman()["info"]["display"]["Raw"] != undefined){
+      if (reg.test(identity.toHuman()["info"]["display"]["Raw"]) == true) {
+        return hexToString(identity.toHuman()["info"]["display"]["Raw"]);
+
+      }else{
+        return identity.toHuman()["info"]["display"]["Raw"];
+
+      }
+    }
+  }
+return node_point;
+
+/*
+  var i = 0;
+  //cy.startBatch();
+  for (nodevar in nodes) {
+    const node_point = nodes[node][0].toHuman()[0]; //nodes in graph
+    const edges = nodes[node][1][0].toHuman(); //node edges/graph connections
+
+    //Adding node points    
+    cy.add([{
+        group: "nodes",
+        data: {
+            id: await checkID(node_point)
+        },
+        position: {
+            x: 0,
+            y: 0
+        }
+    }, ]);
+
+    //And here the deleates
+    for (proxy of edges) {
+        cy.add([{
+            group: "nodes",
+            data: {
+                id: await checkID(proxy.delegate)
+            },
+            position: {
+                x: 0,
+                y: 0
+            }
+        }, ]);
+    }
+    //Here the edges
+    for (proxy of edges) {
+
+                cy.add([{
+                    group: "edges",
+                    data: {
+                        id: proxy.proxyType + i++ + "\n",
+                        source: await checkID(node_point),
+                        target: await checkID(proxy.delegate)
+                    }
+                }, ]);
+    }
+
+      }//end for loop
+
+
+  //await nodesCreation();
+  //await edgeCreation();
+  //cy.endBatch();
+  */
   lay();
 }
 
@@ -204,11 +295,11 @@ function lay() {
   var layout = cy.layout({
     name: 'cola',
     ungrabifyWhileSimulating: true,
-    boundingBox: { x1:0, y1:0, x2:30000, y2:15000 },
+    boundingBox: { x1:0, y1:0, x2:3000, y2:1500 },
     nodeDimensionsIncludeLabels: true,
     randomize: true,
     edgeLength: 100, // sets edge length directly in simulation
-    nodeSpacing: function( node ){ return 1000; },
+    nodeSpacing: function( node ){ return 100; },
     maxSimulationTime: 6000,
   });
 
