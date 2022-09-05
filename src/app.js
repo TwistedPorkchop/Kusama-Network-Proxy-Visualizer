b10388f44043bb41c0c929f9d27b49b07c91eb46
TwistedPorkchop <djerome99@laposte.net>
2022-09-05 04:53:20 +0200

    How much did you write??

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ import { hexToString } from '@polkadot/util';
 
 var cytoscape = require('cytoscape');
 let cola = require('cytoscape-cola');
+import cise from 'cytoscape-cise';
 
+cytoscape.use( cise );
 cytoscape.use( cola ); // register extension
 
 // Construct
@@ -107,21 +109,107 @@ function layJ() {
   cy.fit();
 }
 
-function lay() {
+function layCola() {
   var layout = cy.layout({
     name: 'cola',
     ungrabifyWhileSimulating: true,
-    boundingBox: { x1:0, y1:0, x2:8000, y2:2000 },
+    fit: false,
+    boundingBox: { x1:0, y1:0, x2: 50000, y2: 50000 },
     nodeDimensionsIncludeLabels: true,
     randomize: false,
     edgeLength: 1000, // sets edge length directly in simulation
-    nodeSpacing: function( node ){ return 50; },
-    maxSimulationTime: 6000,
+    nodeSpacing: function( node ){ return 15; },
+    maxSimulationTime: 10000,
   });
 
   layout.run();
-  cy.center();
-  cy.fit();
+  setTimeout(() => { 
+    cy.center();
+    cy.fit();
+  }, 10000);
+  
+};
+
+function lay() {
+  var layout = cy.layout({
+    // -------- Mandatory parameters --------
+    name : 'cise',
+    
+    // ClusterInfo can be a 2D array contaning node id's or a function that returns cluster ids. 
+    // For the 2D array option, the index of the array indicates the cluster ID for all elements in 
+    // the collection at that index. Unclustered nodes must NOT be present in this array of clusters.
+    // 
+    // For the function, it would be given a Cytoscape node and it is expected to return a cluster id  
+    // corresponding to that node. Returning negative numbers, null or undefined is fine for unclustered
+    // nodes.  
+    // e.g
+    // Array:                                     OR          function(node){
+    //  [ ['n1','n2','n3'],                                       ...
+    //    ['n5','n6']                                         }
+    //    ['n7', 'n8', 'n9', 'n10'] ]                         
+    //clusters: clusterInfo,
+    
+    // -------- Optional parameters --------
+    // Whether to animate the layout
+    // - true : Animate while the layout is running
+    // - false : Just show the end result
+    // - 'end' : Animate directly to the end result
+    animate: true,
+    
+    // number of ticks per frame; higher is faster but more jerky
+    refresh: 100, 
+    
+    // Animation duration used for animate:'end'
+    animationDuration: undefined,
+    
+    // Easing for animate:'end'
+    animationEasing: undefined,
+    
+    // Whether to fit the viewport to the repositioned graph
+    // true : Fits at end of layout for animate:false or animate:'end'
+    fit: false,
+    
+    // Padding in rendered co-ordinates around the layout
+    padding: 30,
+    
+    // separation amount between nodes in a cluster
+    // note: increasing this amount will also increase the simulation time 
+    nodeSeparation: 20,
+    
+    // Inter-cluster edge length factor 
+    // (2.0 means inter-cluster edges should be twice as long as intra-cluster edges)
+    idealInterClusterEdgeLengthCoefficient: 2,
+
+    // Whether to pull on-circle nodes inside of the circle
+    allowNodesInsideCircle: false,
+    
+    // Max percentage of the nodes in a circle that can move inside the circle
+    maxRatioOfNodesInsideCircle: 0.5,
+    
+    // - Lower values give looser springs
+    // - Higher values give tighter springs
+    springCoeff: 0.2,
+    
+    // Node repulsion (non overlapping) multiplier
+    nodeRepulsion: 4500,
+    
+    // Gravity force (constant)
+    gravity: 0.25,
+    
+    // Gravity range (constant)
+    gravityRange: 3.8, 
+
+    // Layout event callbacks; equivalent to `layout.one('layoutready', callback)` for example
+    ready: function(){}, // on layoutready
+    stop: function(){}, // on layoutstop
+});
+
+  layout.run();
+  setTimeout(() => { 
+    cy.center();
+    cy.fit();
+  }, 10000);
+  
 };
 
 /*
